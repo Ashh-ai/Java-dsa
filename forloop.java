fa0cb71f22278ce44ceb91f93f57dd71f37be3f6
Ashray Bhasin <ashraybhasin14@gmail.com>
2026-08-18 17:25:22 +0530

    learned basics of  loops

---
+++ forloop.java
@@ -0,0 +1,53 @@
+
+// 1. for loop kya hai?
+// Interview answer:
+// A for loop is a control-flow statement used to repeatedly execute a block of code
+//  as long as a specified condition is true.
+// 18. 🔥 for vs while
+// Interview answer:
+// Both are looping statements. A for loop is generally preferred when initialization, condition, and update are closely related or the iteration pattern is known. 
+// A while loop is generally useful when repetition depends primarily on a condition.
+
+// public class forloop{
+//     public static void main (String args []){
+//         // for ( initialization ; condition; updation){}
+//     for ( int i = 0 ; i <= 5 ; i ++){
+//         System.out.println(i);
+//     }
+ 
+//     }
+    
+// }
+
+
+// // PRINT THE RESVERSE OF THE NUMBER 
+// // n = 1060
+// class reverse{
+//     public ststic main void( String args []);
+//     for( int i = )
+// }
+
+
+
+
+
+// do while loop
+
+// A do-while loop executes the loop body first and checks the condition afterward.
+//  Therefore, it executes at least once.
+// do {
+//     // dosomthing 
+// } while condition;
+
+class dowhile {
+    public static void main ( String args []){
+       int  i = 1;
+        do {
+            System.out.println("hello");
+            i ++;
+        } while( i <= 5 );
+    }
+}
+
+
+// BREAK STATREMENT IS USED TO EXITTHEW LOOP 
---
+++ whileloop.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+// // public class whileloop {
+// //     public static void main( String args []){
+
+// //         int counter = 0;
+// //         while ( counter < 100 ){
+// //         System.out.println("hello worl while loop");
+// //         counter ++;
+// //     }
+
+// //     } 
+// // }
+// public class whileloop {
+
+//     public static void main(String args[]) {
+
+//         int counter = 0;
+
+//         while (counter < 100) {
+//             System.out.println("hello world while loop");
+//             counter++;
+
+//     int c = 1;
+//         while ( c <= 10 ){
+//             System.out.println(c);
+//             c ++;
+//         }
+
+
+//         }
+//     }
+// }
+
+// // print numbers from 1 to 10
+
+
+
+// class nina {
+//     public static void main ( String args[]){
+        
+// Scanner sc = new Scanner ( System.in);
+// System.out.println(" enter the number ");
+// int n = sc.nextInt();
+
+// int counter = 1;
+// while ( counter <= n ){
+//     System.out.println(counter);
+//     counter ++;
+    
+// }
+//     }
+// }
+
+
+class sus {
+    public static  void main ( String args[]){
+      
+          
+    int n = 3;      
+       int  i = 1;
+    int  sum = 0;
+
+while (i  <= n ){
+ 
+    sum = sum+i;
+ i ++;
+}
+ System.out.println(sum);
+    }
+}
+// 1. while loop kya hota hai?
+// Interview answer:
+// A while loop repeatedly executes a block of code as long as the given condition is true.
+//  The condition is checked before every iteration.
