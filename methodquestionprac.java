f301dd0398c1d621556652c51a3fa4fb4128e1b7
Ashray Bhasin <ashraybhasin14@gmail.com>
2026-09-17 18:35:08 +0530

    methodspractise

---
+++ methodquestionprac.java
@@ -0,0 +1,37 @@
+public class methodquestionprac{
+
+
+// Write a Java method to compute the average of three numbers.
+public static double  average(int a ,int b ,int c ){
+    double avg = ( a + b + c) / 3.0;
+    return avg;
+}
+
+// Question 2 : Write a method named isEven that accepts an int argument. The method
+// should return true if the argument is even, or false otherwise. Also write a program to test your
+// method.
+public static boolean checkeven(int n ){
+return n % 2 == 0;
+}
+
+// Question 3 : Write a Java program to check if a number is a palindrome in Java? ( 121 is a
+// palindrome, 321 is not)
+// A number is called a palindrome if the number is equal to the reverse of a number e.g., 121 is a
+// palindrome because the reverse of 121 is 121 itself. On the other hand, 321 is not a
+// palindrome because the reverse of 321 is 123, which is not equal to 321
+
+public static int  checkpalindrome(int n ){
+if n 
+}
+public static  void main(String [] args ){
+
+System.out.println(average(5, 6, 7)); // Ye bilkul valid hai!
+int test1 = 5;
+int test2 = 6; 
+System.out.println(checkeven(test1));
+System.out.println(checkeven(test2));
+
+ 
+} 
+}
+
