e328d25ac836e21bc359341cb5d2bed6ac9d0fc7
Ashray Bhasin <ashraybhasin14@gmail.com>
2026-09-18 12:22:02 +0530

    questions

---
+++ prefixsum.java
@@ -0,0 +1,16 @@
+package todo;
+import java.util.Arrays;
+
+public class prefixsum{
+    public static int[] perfixsum(int[]nums
+
+    }
+    public static void main(string[] args )
+    int [] nums = { 1,2,3,4,5};
+
+    int[] result =prefixsum(nums);
+
+        System.out.println(Arrays.toString(result));
+
+
+}
